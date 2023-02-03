ced4d1bf35464b717a18d4bb409e350ef64e6560
dnieto1997 <daironieto6000@hotmail.com>
2023-02-03 10:26:52 -0500

    primercoomit

---
+++ App.js
@@ -0,0 +1,25 @@
+import 'react-native-gesture-handler'
+import React from 'react'
+import { View,Text} from 'react-native'
+import { NavigationContainer}from '@react-navigation/native'
+import {createStackNavigator} from '@react-navigation/stack'
+
+
+const Stack = createStackNavigator()
+
+
+const App = () => {
+  return (
+    <>
+      
+<NavigationContainer>
+
+
+</NavigationContainer>
+
+
+    </>
+  )
+}
+
+export default App
